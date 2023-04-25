f003331b3392ddc0c560a7eb07b45ae088a86913
david <david@DESKTOP-03H3UL1>
2023-04-25 10:26:06 -0300

    Adicionar arquivos de projeto.

---
+++ ConvertString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a palavra a ser invertida");
+        string palavra = Console.ReadLine();
+        string inverter = "";
+
+        for (int i = palavra.Length - 1; i >= 0; i--)
+        {
+            inverter += palavra[i];
+        }
+
+        Console.WriteLine(inverter);
+    }
+}
